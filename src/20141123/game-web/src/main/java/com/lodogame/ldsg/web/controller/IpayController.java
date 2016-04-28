bbe0615fab3242be8108f0169b91e1b7f0d0efb1
dogdog7788 <dogdog7788@qq.com>
2016-04-28 14:53:03 +0800

    no message

--- src/20141123/game-web/src/main/java/com/lodogame/ldsg/web/controller/IpayController.java
+++ src/20141123/game-web/src/main/java/com/lodogame/ldsg/web/controller/IpayController.java
@@ -14,16 +14,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSONObject;
-import com.lodogame.ldsg.partner.model.ipay.IAppPaySDKConfig;
 import com.lodogame.ldsg.partner.model.ipay.SignHelper;
 import com.lodogame.ldsg.partner.service.PartnerService;
 import com.lodogame.ldsg.web.factory.PartnerServiceFactory;
 @Controller
 public class IpayController {
-private static Logger LOG = Logger.getLogger(HucnController.class);
+private static Logger LOG = Logger.getLogger(IpayController.class);
 	
 	@Autowired
 	private PartnerServiceFactory serviceFactory;
+	
+	public final static String PLATP_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCApMdoweZ/kw9lyQMjmjFGucW8Dz5T9Dlg2JFNHH00zIstRtHaCPoonzZEwNusC9dNE39WP3p5P1OwX+eILXpmi/d5k76FOVnINRsouljslWwKMVYKLumeGd2TB4YI2E2eKIoHvULyrfiUx1xAXkajTAN/JpF7V51us5sv0Fwr7wIDAQAB";
+
 
 	@RequestMapping(value = "/webApi/ipayPayment.do", method = RequestMethod.POST)
 	public ModelAndView payCallback(HttpServletRequest req, HttpServletResponse res) {
@@ -39,7 +41,7 @@ private static Logger LOG = Logger.getLogger(HucnController.class);
 		 * 
 		 * 主要 目的 确定 收到的数据是我们 发的数据，是没有被非法改动的
 		 */
-		if (SignHelper.verify(transdata, sign, IAppPaySDKConfig.PLATP_KEY)) {
+		if (SignHelper.verify(transdata, sign, PLATP_KEY)) {
 			LOG.info("校验成功");
 			JSONObject json = JSONObject.parseObject(transdata);
 			String orderId = json.getString("cporderid");
